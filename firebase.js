9e6bdb0690cc030c7b37411d09d84b8135c9989e
ranjanrai <43703789+ranjanrai@users.noreply.github.com>
2026-02-21 15:06:55 +0530

    Refactor Firebase setup and add auth service
    Updated Firebase configuration and initialized required services.

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAbNAsb2kYTRHPR9vbiZ3eWqnK-Hkan3Q0",
   authDomain: "id-verification-fa673.firebaseapp.com",
   projectId: "id-verification-fa673",
-  storageBucket: "id-verification-fa673.firebasestorage.app",
+  storageBucket: "id-verification-fa673.appspot.com",
   messagingSenderId: "306500578530",
-  appId: "1:306500578530:web:f4f6e22834a3257f4e02bb",
-  measurementId: "G-TQ32MHTR3F"
+  appId: "1:306500578530:web:f4f6e22834a3257f4e02bb"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// ✅ Declare db ONLY HERE
+// ✅ REQUIRED SERVICES
+const auth = firebase.auth();
 const db = firebase.firestore();
-
 const storage = firebase.storage();
