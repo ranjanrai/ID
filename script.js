f62f29fe0d9f0ae546251859f6fb68c6e873895c
ranjanrai <43703789+ranjanrai@users.noreply.github.com>
2026-03-07 15:33:27 +0530

    Refactor PDF text extraction and student search logic

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 let pdfText = "";
 
-pdfjsLib.getDocument("students.pdf").promise.then(function(pdf) {
+pdfjsLib.getDocument("students.pdf").promise.then(function(pdf){
 
-```
 let pages = [];
 
-for (let i = 1; i <= pdf.numPages; i++) {
+for(let i=1;i<=pdf.numPages;i++){
 
-    pages.push(
-        pdf.getPage(i).then(function(page) {
+pages.push(
+pdf.getPage(i).then(function(page){
 
-            return page.getTextContent().then(function(text) {
+return page.getTextContent().then(function(content){
 
-                return text.items.map(item => item.str).join(" ");
+return content.items.map(item => item.str).join(" ");
 
-            });
+});
 
-        })
-    );
+})
+);
 
 }
 
-Promise.all(pages).then(function(texts) {
+Promise.all(pages).then(function(texts){
 
-    pdfText = texts.join(" ");
-    console.log(pdfText);
+pdfText = texts.join(" ");
+console.log(pdfText);
 
 });
-```
 
 });
 
@@ -41,7 +39,7 @@ result.innerHTML="Please enter student name";
 return;
 }
 
-let records = pdfText.split("ID CARD NO");
+let records = pdfText.split("RCPHS");
 
 let found=false;
 
@@ -51,29 +49,22 @@ if(record.toUpperCase().includes(name)){
 
 found=true;
 
-let id = record.match(/RCPHS/[0-9/A-Z]+/);
-let course = record.match(/Course\s*:\s*[A-Z]+/i);
-let session = record.match(/Session\s*:\s*[0-9-]+/i);
-let father = record.match(/F.\s*Name\s*:\s*[A-Z ]+/i);
-let mother = record.match(/M.\s*Name\s*:\s*[A-Z ]+/i);
-let pin = record.match(/Pin\s*-\s*[0-9]+/i);
+let idMatch = record.match(/RCPHS/[0-9/A-Z]+/);
+let courseMatch = record.match(/Course\s*:\s*[A-Z]+/i);
+let sessionMatch = record.match(/Session\s*:\s*[0-9-]+/i);
 
-result.innerHTML =
-"<h2 style='color:green'>ID VERIFIED</h2>" +
+let course = courseMatch ? courseMatch[0].replace(/Course\s*:\s*/i,"") : "";
+let session = sessionMatch ? sessionMatch[0].replace(/Session\s*:\s*/i,"") : "";
+let id = idMatch ? idMatch[0] : "";
 
-"<b>ID Card:</b> "+ (id ? id[0] : "") +"<br><br>" +
+result.innerHTML = `
 
-"<b>Name:</b> "+ name +"<br><br>" +
-
-"<b>Course:</b> "+ (course ? course[0].replace(/Course\s*:\s*/i,"") : "") +"<br>" +
-
-"<b>Session:</b> "+ (session ? session[0].replace(/Session\s*:\s*/i,"") : "") +"<br>" +
-
-"<b>Father Name:</b> "+ (father ? father[0].replace(/F.\s*Name\s*:\s*/i,"") : "") +"<br>" +
-
-"<b>Mother Name:</b> "+ (mother ? mother[0].replace(/M.\s*Name\s*:\s*/i,"") : "") +"<br>" +
-
-"<b>Pin:</b> "+ (pin ? pin[0] : "");
+<h2 style="color:green">ID VERIFIED</h2>
+<b>ID Card No:</b> ${id} <br><br>
+<b>Name:</b> ${name} <br><br>
+<b>Course:</b> ${course} <br>
+<b>Session:</b> ${session}
+`;
 
 }
 
