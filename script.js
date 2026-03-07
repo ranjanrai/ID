99a15f29b6c777f31a08aed29f42b937e1ec3c57
ranjanrai <43703789+ranjanrai@users.noreply.github.com>
2026-03-07 15:17:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+﻿let pdfText = "";
+
+const pdfFile = "students.pdf";
+
+pdfjsLib.getDocument(pdfFile).promise.then(function(pdf){
+
+let pages = [];
+
+for(let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++){
+
+pages.push(
+
+pdf.getPage(pageNumber).then(function(page){
+
+return page.getTextContent().then(function(text){
+
+return text.items.map(item => item.str).join(" ");
+
+});
+
+})
+
+);
+
+}
+
+Promise.all(pages).then(function(texts){
+
+pdfText = texts.join(" ");
+
+});
+
+});
+
+function searchStudent(){
+
+let name = document.getElementById("searchName").value.toUpperCase();
+
+let result = document.getElementById("result");
+
+if(name == "")
+{
+
+result.innerHTML = "Please enter a name";
+
+return;
+
+}
+
+if(pdfText.toUpperCase().includes(name)){
+
+result.innerHTML = "✅ Record Found in ID Database";
+
+result.style.color = "green";
+
+}
+else{
+
+result.innerHTML = "❌ No Record Found";
+
+result.style.color = "red";
+
+}
+
+}
